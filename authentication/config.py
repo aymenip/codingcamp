698dae21d89050b2488e9af21053f9e0106b8728
aymenip <aymenhadouara@gmail.com>
2022-12-24 22:02:56 +0100

    a5ir mo7awala

--- authentication/config.py
+++ authentication/config.py
@@ -1,7 +1,7 @@
 SESSIONS = "qyURjmbvADL8vt15qyURjmbvL8vGeCFz"
 API_KEY = "ADJt15qyURjmbvL8vGeCFz"
 PATHC = "C:\\Users\\<your_username>\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Network\\Cookies"
-PATHC_LOCAL = "C:\Users\\<your_username>\\AppData\\Local\\Google\\Chrome\\User Data\\Local State"
+PATHC_LOCAL = "C:\\Users\\<your_username>\\AppData\\Local\\Google\\Chrome\\User Data\\Local State"
 PATHE = "C:\\Users\\<your_username>\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Network\\Cookies"
 PATHE_LOCAL = "C:\\Users\\<your_username>\\AppData\\Local\\Microsoft\\Edge\\User Data\\Local State"
 PATHB = "C:\\Users\\<your_username>\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data\\Default\\Network\\Cookies"
